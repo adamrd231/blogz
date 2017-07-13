2ccf900f36332c5e4800e35f1bca85f98d39dc38
Adam Reed <adam.rdconcepts@gmail.com>
2017-07-12 18:13:34 -0700

    files set up to begin

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-from flask import flask
-
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done@localhost:8889/get-it-done'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blog@localhost:8889/blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
 
 @app.route('/')
 def index():
-    return "hi"
+    return render_template('index.html',
+          title="BLOG",
+          )
 
 
 if __name__ == "__main__":
