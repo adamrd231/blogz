1832184e09fcfb1aae09f8bcfe48b3ea1313a1ae
Adam Reed <adam.rdconcepts@gmail.com>
2017-07-14 13:46:33 -0700

    add login and register pages

--- main.py
+++ main.py
@@ -6,26 +6,53 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogz@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+
+#TODO figure out what the heck this secret key is
 app.secret_key = "abc"
 
+
+#Create class blogs, making them persistent and attaching the information to the database
 class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(46))
     content = db.Column(db.String(140))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
+    #Instantiate the attributes of our blog object
     def __init__(self, title, content):
         self.title = title
         self.content = content
 
+    #Keep helper functions inside of the class, this one checks that you have something in both title and content.
     def is_valid(self):
         if self.title and self.content:
             return True
         else:
             return False
 
+class User(db.Model):
 
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True)
+    password = db.Column(db.String(15))
+
+    #TODO I am not super clear on this realtionship part, especially the backref portion
+    blogs = db.relationship('Blog', backref='owner')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+
+#Reroute from home index to the home page
 @app.route('/', methods=['POST', 'GET'])
+def re_direct():
+    return redirect('/blog')
+
+
+#THIS IS THE HOME PAGE
+@app.route('/blog', methods=['POST', 'GET'])
 def index():
 
     blogs = Blog.query.all()
@@ -35,6 +62,50 @@ def index():
           blogs=blogs,
           )
 
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['email']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+            session['username'] = username
+            flash("Logged in")
+            redirect('/new_post')
+        else:
+            flash('User name or password incorrect, or does not exist.')
+
+    return render_template('login.html')
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify']
+
+        # - Validate the users data
+        existing_user = User.query.filter_by(username=username).first()
+
+
+        if not existing_user:
+            new_user = User(username, password)
+            db.session.add(new_user)
+            db.session.commit()
+            #todo - Remember the user
+            session['username'] = username
+            flash("logged in")
+            return redirect('/new_post')
+        else:
+            # todo - user better response message
+            return "<h1>Duplicate User</h1>"
+
+    return render_template('register.html')
+
+
+#DELETE TASKS OUT OF THE BLOG, THIS IS A BUTTON NEXT TO THE BLOG ON THE HOMEPAGE
 @app.route('/delete-task', methods=['POST'])
 def delete_task():
 
@@ -46,6 +117,8 @@ def delete_task():
 
     return redirect('/')
 
+
+#CREATE A NEW POST, BRINGS YOU TO A NEW PAGE
 @app.route('/new_post', methods=['POST', 'GET'])
 def new_post():
 
@@ -74,6 +147,7 @@ def new_post():
 
 
 
+#TEMPLATE TO CREATE BLOG PAGES, THIS IS REUSED FOR EVERY BLOG ENTRY.
 @app.route('/single_template', methods=['GET'])
 def single_template():
 
